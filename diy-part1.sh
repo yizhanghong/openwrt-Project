fb7028d5572e0dd36169d92ef9d2dad9cc0a67ff
firkerword <61251923+firkerword@users.noreply.github.com>
2020-09-01 16:42:24 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,4 +29,4 @@ svn co https://github.com/xiaorouji/openwrt-package/trunk/package/chinadns-ng pa
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/tcping package/tcping
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-go package/trojan-go
 svn co https://github.com/xiaorouji/openwrt-package/trunk/package/trojan-plus package/trojan-plus
-svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy package/https-dns-proxy
+svn co https://github.com/coolsnowwolf/packages/trunk/net/https-dns-proxy package/https-dns-proxys
