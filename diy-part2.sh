f9ee5ac39e15c5f119cf25a1bd1a5d3dd8bf4aef
firkerword <61251923+firkerword@users.noreply.github.com>
2020-10-26 18:56:46 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,20 +11,23 @@
 
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/lang/golang/golang/Makefile -O ./feeds/packages/lang/golang/golang/Makefile
-wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/Makefile -O ./feeds/packages/libs/libcap/Makefile
-wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/patches/300-disable-tests.patch -O ./feeds/packages/libs/libcap/patches/300-disable-tests.patch
 wget  https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/v2ray/Makefile -O ./package/lean/v2ray/Makefile
 wget  https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/v2ray-plugin/Makefile -O ./package/lean/v2ray-plugin/Makefile
+# wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/Makefile -O ./feeds/packages/libs/libcap/Makefile
+# wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/patches/300-disable-tests.patch -O ./feeds/packages/libs/libcap/patches/300-disable-tests.patch
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/v2fly/master/v2ray-plugin/Makefile > ./package/lean/v2ray-plugin/Makefile
 # curl -fsSL  https://raw.githubusercontent.com/firkerword/v2fly/master/v2ray/Makefile > ./package/lean/v2ray/Makefile
 # curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
 # cp -r feeds/lienol/lienol/luci-app-passwall feeds/diy
 # cp -r feeds/lienol/package/{chinadns-ng,trojan-go,syncthing,tcping} feeds/diy/vssr
 # rm -rf ./feeds/{lienol,lienol.tmp}
-rm -rf ./feeds/packages/libs/libcap/patches/100-portability.patch
-rm -rf ./feeds/packages/libs/libcap/patches/200-change-hardcoded-shell-to-sh.patch
+# rm -rf ./feeds/packages/libs/libcap/patches/100-portability.patch
+# rm -rf ./feeds/packages/libs/libcap/patches/200-change-hardcoded-shell-to-sh.patch
+
 rm -rf ./package/lean/luci-theme-argon
 rm -rf ./package/lienol/v2ray
 rm -rf ./package/lienol/v2ray-plugin
 rm -rf ./feeds/packages/net/smartdns
 rm -rf ./feeds/packages/net/https-dns-proxy
+rm -rf ./feeds/packages/libs/libcap
+
