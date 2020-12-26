65f12c46d51c6b5bf2691c64c8023c04e7e80360
firkerword <61251923+firkerword@users.noreply.github.com>
2020-12-26 15:20:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,27 +10,9 @@
 # Modify default IP
 
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-# wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/lang/golang/golang/Makefile -O ./feeds/packages/lang/golang/golang/Makefile
-# wget  https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/v2ray/Makefile -O ./package/lean/v2ray/Makefile
-# wget  https://raw.githubusercontent.com/coolsnowwolf/lede/master/package/lean/v2ray-plugin/Makefile -O ./package/lean/v2ray-plugin/Makefile
-# wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/Makefile -O ./feeds/packages/libs/libcap/Makefile
-# wget  https://raw.githubusercontent.com/coolsnowwolf/packages/master/libs/libcap/patches/300-disable-tests.patch -O ./feeds/packages/libs/libcap/patches/300-disable-tests.patch
-# curl -fsSL  https://raw.githubusercontent.com/firkerword/v2fly/master/v2ray-plugin/Makefile > ./package/lean/v2ray-plugin/Makefile
-# curl -fsSL  https://raw.githubusercontent.com/firkerword/v2fly/master/v2ray/Makefile > ./package/lean/v2ray/Makefile
-# curl -fsSL  https://raw.githubusercontent.com/Lienol/openwrt-packages/19.07/net/https-dns-proxy/files/https-dns-proxy.init > ./feeds/packages/net/https-dns-proxy/files/https-dns-proxy.init
-# cp -r feeds/lienol/lienol/luci-app-passwall feeds/diy
-# cp -r feeds/lienol/package/{chinadns-ng,trojan-go,syncthing,tcping} feeds/diy/vssr
-# rm -rf ./feeds/{lienol,lienol.tmp}
 
-rm -rf ./package/lean/v2ray
-rm -rf ./package/lean/v2ray-plugin
 rm -rf ./package/lean/luci-theme-argon
-rm -rf ./package/lienol/v2ray
-rm -rf ./package/lienol/v2ray-plugin
 rm -rf ./feeds/packages/net/smartdns
-rm -rf ./feeds/packages/net/https-dns-proxy
-rm -rf ./feeds/packages/lang/golang
 
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray package/lean/v2ray
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/v2ray-plugin package/lean/v2ray-plugin
-svn co https://github.com/coolsnowwolf/packages/trunk/lang/golang feeds/packages/lang/golang
+
+
