18bd8857a20b2f380d4bb31dc20718abb4a2ee21
firkerword <61251923+firkerword@users.noreply.github.com>
2022-04-06 23:23:17 +0800

    Update openwrt-Project.sh

--- openwrt-Project.sh
+++ openwrt-Project.sh
@@ -15,6 +15,7 @@
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
+git clone git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
 git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
