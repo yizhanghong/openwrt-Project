3d628608511f49826c28d11f2b0dc54bebe7e2a9
firkerword <61251923+firkerword@users.noreply.github.com>
2022-04-10 07:08:31 +0800

    Update openwrt-Project.sh

--- openwrt-Project.sh
+++ openwrt-Project.sh
@@ -31,4 +31,4 @@ git clone -b master https://github.com/vernesong/OpenClash.git package/OpenClash
 svn co https://github.com/linkease/istore/trunk/luci/luci-app-store package/luci-app-store
 svn co https://github.com/linkease/istore-ui/trunk/app-store-ui package/app-store-ui
 svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt package/aliyundrive-webdav
-svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
