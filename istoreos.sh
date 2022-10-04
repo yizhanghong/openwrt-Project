f5ce5f4163f7b7d2b20f11cba67711a213a5a901
firkerword <61251923+firkerword@users.noreply.github.com>
2022-10-04 23:01:34 +0800

    Create istoreos.sh

---
+++ istoreos.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
+git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
