68c62f2a8603033a27907f2fb21f3ff7e49a15fd
firkerword <61251923+firkerword@users.noreply.github.com>
2022-10-05 07:57:37 +0800

    Update istoreos.sh

--- istoreos.sh
+++ istoreos.sh
@@ -3,5 +3,5 @@ sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;ma
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
 git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
 git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
+# git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
 svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
