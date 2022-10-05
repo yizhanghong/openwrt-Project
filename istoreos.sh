ea1e0527fe00209cbfc2502b19b160feeb15997e
firkerword <61251923+firkerword@users.noreply.github.com>
2022-10-06 00:02:06 +0800

    Update and rename istoreos.sh to LienolOSsh

--- istoreos.sh
+++
@@ -1,7 +0,0 @@
-sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
-git clone -b luci https://github.com/xiaorouji/openwrt-passwall package/passwall
-# git clone https://github.com/QiuSimons/openwrt-mos.git package/openwrt-mos
-# git clone https://github.com/sbwml/luci-app-alist.git package/luci-app-alist
-svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
