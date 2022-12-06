3b0de5544e945dcb24dd8d64a088b375a590745f
firkerword <61251923+firkerword@users.noreply.github.com>
2022-12-07 00:48:54 +0800

    Update LienolOS2.sh

--- LienolOS2.sh
+++ LienolOS2.sh
@@ -17,8 +17,7 @@ curl -fsSL https://raw.githubusercontent.com/coolsnowwolf/luci/master/applicatio
 curl -fsSL  https://raw.githubusercontent.com/firkerword/KPR/main/cus_config.yaml > ./package/openwrt-mos/luci-app-mosdns/root/etc/mosdns/cus_config.yaml
 # Modify default IP
 # sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-
-sed -i 's/bootstrap/argon/g' ./feeds/luci/modules/luci-base/root/etc/config/luci
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
 rm -rf ./package/diy-ziyong/theme
 rm -rf ./package/diy-ziyong/luci-app-wrtbwmon
 rm -rf ./package/diy-ziyong/wrtbwmon
