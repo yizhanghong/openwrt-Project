17daa506df451435bde75918216bfd254456bbce
firkerword <61251923+firkerword@users.noreply.github.com>
2023-02-17 10:35:17 +0800

    Update lienol19072.sh

--- lienol19072.sh
+++ lienol19072.sh
@@ -12,8 +12,12 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+# 替换
 rm -rf ./feeds/packages/lang/golang
 svn export https://github.com/sbwml/packages_lang_golang/branches/19.x feeds/packages/lang/golang
 rm -rf feeds/packages/libs/libcap
 svn co https://github.com/openwrt/packages/branches/openwrt-21.02/libs/libcap/ feeds/packages/libs/libcap
-
+rm -rf feeds/packages/net/miniupnpd
+svn co https://github.com/coolsnowwolf/packages/trunk/net/miniupnpd feeds/packages/net/miniupnpd
+rm -rf feeds/luci/applications/luci-app-upnp
+https://github.com/coolsnowwolf/luci/tree/master/applications/luci-app-upnp feeds/luci/applications/luci-app-upnp
