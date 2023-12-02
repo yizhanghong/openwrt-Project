3271944b56d5cf3c72332cb704db37f92cf31e29
firkerword <61251923+firkerword@users.noreply.github.com>
2023-12-02 22:42:49 +0800

    Update LienolOS.sh

--- LienolOS.sh
+++ LienolOS.sh
@@ -26,7 +26,7 @@ git clone https://github.com/fw876/helloworld.git package/ssr
 git clone https://github.com/firker/diy-ziyong.git package/diy-ziyong
 # git clone https://github.com/tty228/luci-app-serverchan.git package/luci-app-serverchan
 # git clone https://github.com/kiddin9/openwrt-bypass.git package/openwrt-bypass
-git clone -b packages https://github.com/xiaorouji/openwrt-passwall.git package/openwrt-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall-packages.git package/openwrt-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall.git package/passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall2.git package/passwall2
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
