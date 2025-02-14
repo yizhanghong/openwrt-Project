b36cf4d5814407b5eb93a82edd2f3a4c3fa10559
yizhanghong <865362414@qq.com>
2025-02-14 13:42:00 +0800

    Update openwrt-immortalwrt2.sh
    update golang 23.x

--- openwrt-immortalwrt2.sh
+++ openwrt-immortalwrt2.sh
@@ -30,4 +30,4 @@ rm -rf ./feeds/luci/applications/luci-app-openclash
 rm -rf ./feeds/luci/applications/luci-app-smartdns
 # rm -rf ./feeds/luci/applications/luci-app-adbyby-plus
 rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 22.x feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 23.x feeds/packages/lang/golang
